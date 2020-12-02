d77929ad74b9044e39307fdcdf838a1f96990a3b
Nurdaulet <nurik9293709@gmail.com>
2020-12-02 19:27:13 +0600

    with trashed fix

--- src/resources/views/actions/relation_deleted.blade.php
+++ src/resources/views/actions/relation_deleted.blade.php
@@ -1,6 +1,6 @@
 @php
     $data = $revision->old;
-    $related = $revision->revisionable->{$data['relation']}()->withTrashed()->find($data['id']);
+    $related = $revision->revisionable->{$data['relation']}()->find($data['id']);
 @endphp
 @if($related)
 с <a href="{{ $related->identifiableLink() }}">{{ $related->name }}</a>
